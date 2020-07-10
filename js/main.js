0e476f2ab54b646feed4c38b5ec8a7b3c81e4f04
Ashley Campbell <ashleycampbell@MacBook-Pro.lan>
2020-07-10 14:52:06 +0100

    fetch js working!

--- js/main.js
+++ js/main.js
@@ -1,8 +1,48 @@
 
 // ------ Variables ----------- //
 let employees = [];
+const employeeContainer = document.querySelector('.container');
+const overlay = employeeContainer.querySelector('.overlay');
 const url = 'https://randomuser.me/api/?results=12';
+const modalContent = document.querySelector('.modal-content');
+const closeModal = document.querySelector('.modal-close');
 
-fetch(url)
-    .then(data => data.json())
-    .then(data => console.log(data.results[0].name.first))
+async function getJson(url) {
+    try {
+        const response = await fetch(url);
+        return await response.json();
+    }
+    catch (error) {
+        throw error;
+    }
+};
+
+const displayEmployees = data => {
+    let employeeHTML = '';
+    data.forEach( data => {
+        let name = `${data.name.first} ${data.name.last}`;       
+        let email = data.email;
+        let city = data.location.city;
+        let picture = data.picture.medium;
+        employeeHTML += `
+        <div class= 'employee'>
+            <div class= 'flex-card'>
+                <img src='${picture}'>
+                <div class= 'flex-list'>
+                    <h2>${name}</h2>
+                    <span id= 'email'>${email}</span>
+                    <span>${city}</span>
+                </div>
+            </div>
+        </div>
+        `
+    })
+    employeeContainer.innerHTML = employeeHTML;
+}
+
+getJson(url)
+    .then(data => data.results)
+    // .then(data => console.log(data));
+    .then(data => displayEmployees(data));
+
+ 
